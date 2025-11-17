d39802545d7a7be93a3cba76bb4463554d7d531e
Diego Emilio Ramirez Garcia <diego.emilio.raga@gmail.com>
2025-11-17 17:55:05 -0600

    modelos de sucursal y auto

--- cafeteria/models.py
+++ cafeteria/models.py
@@ -30,3 +30,22 @@ class Contacto(models.Model):
 
     def __str__(self):
         return f"{self.asunto} - {self.nombre}"
+
+class Auto(models.Model):
+    marca = models.CharField(max_length=50)              
+    modelo = models.CharField(max_length=50)             
+    año = models.PositiveIntegerField()                  
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+class Sucursal(models.Model):
+    nombre = models.CharField(max_length=100)
+    calle = models.CharField(max_length=200)
+    colonia = models.CharField(max_length=200)
+    ciudad = models.CharField(max_length=100)
+    codigoPostal = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre}\n{self.calle}, {self.colonia}\n{self.ciudad}\n{self.codigoPostal}"
