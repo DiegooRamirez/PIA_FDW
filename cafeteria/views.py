7c9a3e9051ec3a77fb96fb7d4ac10edad0cbbe13
nowadaystudiomx-code <nowadaystudio@gmail.com>
2025-11-16 19:49:36 -0600

    Subiendo push por que mariana me corrijio

--- cafeteria/views.py
+++ cafeteria/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request): 
+    return render(request, "index.html")
+
+def menu(request):
+    return render(request, "menu.html")
+
+def sucursales(request):
+    return render(request, "sucursales.html")
+
+def contacto(request):
+    return render(request, "contacto.html")
+
+def opiniones(request):
+    return render(request, "opiniones.html")
