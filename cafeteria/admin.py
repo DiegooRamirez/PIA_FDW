b90893b6070cb77d7b361df26fa2ac6c120ffea4
nowadaystudiomx-code <nowadaystudio@gmail.com>
2025-11-17 20:05:15 -0600

    Arreglando forms y quitando mapa de contacto

--- cafeteria/admin.py
+++ cafeteria/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from .models import Auto, Sucursal, Producto, Evento, Opinion
+from .models import Auto, Sucursal, Producto, Evento, Opinion, Contacto
 
 admin.site.register(Auto)
 admin.site.register(Sucursal)
 admin.site.register(Producto)
 admin.site.register(Evento)
 admin.site.register(Opinion)
+
+@admin.register(Contacto)
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ('asunto', 'nombre', 'email', 'fecha', 'leido')
+    list_filter = ('leido', 'fecha')
+    search_fields = ('nombre', 'email', 'asunto', 'mensaje')
+    readonly_fields = ('fecha',)
+
